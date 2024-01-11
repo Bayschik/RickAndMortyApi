97d30c99cc7306b61e40468672b8b2002100f119
Baychik <ergeshovbaistan@gmail.com>
2024-01-11 23:04:05 +0600

    implemented base classes

--- app/src/main/java/com/example/hw_2_6/data/Repository.kt
+++ app/src/main/java/com/example/hw_2_6/data/Repository.kt
@@ -1,37 +1,15 @@
 package com.example.hw_2_6.data
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.liveData
-import kotlinx.coroutines.Dispatchers
+import com.example.hw_2_6.ui.base.BaseRepository
 
-class Repository(private val api: CartoonApiService) {
+class Repository(private val api: CartoonApiService):BaseRepository(api) {
 
-    fun getCharacters(): LiveData<Resource<List<Character>>> = liveData(Dispatchers.IO) {
-        emit(Resource.Loading())
-        try {
-            val response = api.getCharacters()
-            if (response.isSuccessful && response.body() != null) {
-                response.body()?.let {
-                    emit(Resource.Success(it.results))
-                }
-            }
-        } catch (e: Exception) {
-            emit(Resource.Error(e.localizedMessage ?: "Unknown error"))
-        }
+    fun getCharacters(): LiveData<Resource<List<Character>>> = performRequest {
+        api.getCharacters().body()?.results ?: emptyList()
     }
 
-    fun getCharacterDetails(id: Int): LiveData<Resource<Character>> = liveData(Dispatchers.IO) {
-        try {
-            val cartoon = api.getCharacterDetails(id)
-            if (cartoon.isSuccessful) {
-                cartoon.body()?.let {
-                    emit(Resource.Success(it))
-                }
-            }
-        } catch (ex: Exception) {
-            emit(Resource.Error(ex.localizedMessage ?: "Unknown Error"))
-        }
-
+    fun getCharacterDetails(id: Int): LiveData<Resource<Character>> = performRequest {
+        api.getCharacterDetails(id).body()!!
     }
-
 }
--- app/src/main/java/com/example/hw_2_6/ui/base/BaseRepository.kt
+++ app/src/main/java/com/example/hw_2_6/ui/base/BaseRepository.kt
@@ -1,4 +1,22 @@
 package com.example.hw_2_6.ui.base
 
-class BaseRepository {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import com.example.hw_2_6.data.CartoonApiService
+import com.example.hw_2_6.data.Resource
+import kotlinx.coroutines.Dispatchers
+
+abstract class BaseRepository(private val api: CartoonApiService) {
+
+    protected fun <T> performRequest(apiCall: suspend () -> T): LiveData<Resource<T>> =
+        liveData(Dispatchers.IO) {
+            emit(Resource.Loading())
+            try {
+                val response = apiCall.invoke()
+                emit(Resource.Success(response))
+            } catch (e: Exception) {
+                emit(Resource.Error(e.localizedMessage ?: "Unknown error"))
+            }
+        }
+
 }
